6615c8a91f7aabd813d85209cedd1d815b3ac4b3
kyrianmirian <kyrianmirian@gmail.com>
2022-12-09 10:23:10 -0800

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: Prints to the stdout using puts
+ *     * Return: Always 0 (Success)
+ *      */
+int main(void)
+{
+
+	char my_prnt[] = ("\"Programming is like building a multilingual puzzle");	
+	return (0);
+} 
