9ce64ec8ab89fec9559c40214a4b3e1cf1fce985
kyrianmirian <kyrianmirian@gmail.com>
2022-12-10 05:46:12 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,11 @@
  * main - Entry point
  *
  * Description: using the main function 
- * this program prints "with proper grammar, but the outcome is a piece of art,"
- * Return: 0
+ * this program prints with proper grammar, but the outcome is a piece of art,
+ * Return`
  */
 int main(void)
 {
-	printf("\ with proper grammar, but the outcome is a piece of art,"):
+	printf("\ with proper grammar, but the outcome is a piece of art,");
 		return (0);
 }
