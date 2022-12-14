007e098328759fa931f265f4fff12200332bc2b4
kyrianmirian <kyrianmirian@gmail.com>
2022-12-13 20:36:06 -0800

    824

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - compute the absolute valuce of an integer
+ * @n: is the number
+ * Return: return the computed value
+ */
+
+int _abs(int n)
+{
+return(ab(n));
+}
